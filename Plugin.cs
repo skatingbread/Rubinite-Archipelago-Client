2e5638f0f385d992d26c718aa5c73d143687fc4f
skatingbee <abekesting@gmail.com>
2026-08-18 21:04:56 -0500

    Fixed plugin name

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,7 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 
-namespace TempFolder;
+namespace RubiniteArchipelagoClient;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
