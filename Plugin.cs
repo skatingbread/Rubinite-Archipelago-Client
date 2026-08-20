6fe26ef1eac0b075570565bf4b92007e21df8e59
skatingbee <abekesting@gmail.com>
2026-08-19 21:27:07 -0500

    cursed test to see if connection works

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,7 @@
-﻿using BepInEx;
+﻿using System;
+using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.Enums;
+using BepInEx;
 using BepInEx.Logging;
 
 namespace Rubinite_Archipelago_Client;
@@ -13,5 +16,21 @@ public class Plugin : BaseUnityPlugin
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        //Temp Test Code (Very cursed delete later
+        Logger.LogInfo("Connecting to AP and lying about it");
+        try
+        {
+            ArchipelagoSession session = ArchipelagoSessionFactory.CreateSession("archipelago.gg", 39353);
+
+            LoginResult result = session.TryConnectAndLogin("Refunct", "abefunct", ItemsHandlingFlags.NoItems);
+            session.Say("Test");
+        } catch (Exception e)
+        {
+            Logger.LogError(e);
+            Logger.LogInfo($"Failed to connect {e}");
+        }
+        Logger.LogInfo("Connected and Said Test");
+
     }
 }
