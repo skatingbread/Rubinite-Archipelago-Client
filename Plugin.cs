caa951f04d04c142b95c71b5a8e0dd386d29a43d
skatingbee <abekesting@gmail.com>
2026-09-17 23:51:52 -0500

    finish plugin startup logic

--- Plugin.cs
+++ Plugin.cs
@@ -1,17 +1,20 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
 
 namespace Rubinite_Archipelago_Client;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
+    private readonly Harmony _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
     internal static new ManualLogSource Logger;
         
     private void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
+        _harmony.PatchAll();
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
 }
